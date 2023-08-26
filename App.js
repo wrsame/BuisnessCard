83a79f19fc41686a3bc1d7db5c79f6f7dd400f71
wrsame <jawa0262@stud.kea.dk>
2023-08-26 22:08:33 +0200

    BCard done

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  
+<View style={styles.container}>
+  <View style={styles.card}>
+  <Image
+    style={styles.profileImage}
+    source={{
+    uri: 'https://logos-world.net/wp-content/uploads/2020/04/Nike-Logo.png',
+    }} 
+    resizeMode="contain"
+  />
+      <Text style={styles.name}>Jawaahir Warsame</Text>
+      <Text style={styles.jobTitle}>Software Developer</Text>
+      <Text style={styles.contact}>Email: jawa0262@stud.kea.dk</Text>
+      <Text style={styles.contact}>Phone: (+45) 456-7890</Text>
+    </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+
+container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'darkblue'
   },
+  card: {
+    backgroundColor: '#b2ebf2',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 10,
+    textAlign: 'center',
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  jobTitle: {
+    fontSize: 18,
+    marginBottom: 16,
+    textAlign: 'center',
+  },
+  contact: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+
+
+  profileImage:{
+    width: 120, 
+    height: 120, 
+    alignSelf: 'center'
+  }
 });
